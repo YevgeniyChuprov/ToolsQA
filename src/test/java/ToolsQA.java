2e20a50ea5a5995486eec8231eab6cabe14ffa9f
Yevgeniy Chuprov <yevgeniy.chuprov@nedra.digital>
2022-11-29 19:52:18 +0300

    first commit

---
+++ src/test/java/ToolsQA.java
@@ -0,0 +1,55 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ToolsQA {
+
+    @BeforeAll
+    static public void BeforeAll(){
+        Configuration.browserSize = "1920x1080";
+        open("https://demoqa.com/automation-practice-form");
+    }
+
+    @Test
+    public void test(){
+        $("#firstName").setValue("Evgeny");
+        $("#lastName").setValue("Сhuprov");
+        $("#userEmail").setValue("myemail@mail.my");
+        $("[for='gender-radio-1']").click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1985");
+        $(".react-datepicker__day--023").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("[for='hobbies-checkbox-1']").click();
+        $("[for='hobbies-checkbox-3']").click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/1.png"));
+        $("#currentAddress").setValue("Some kind of address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
+                text("Evgeny Сhuprov"),
+                text("myemail@mail.my"),
+                text("Male"),
+                text("1234567890"),
+                text("23 February,1985"),
+                text("Maths"),
+                text("Sports, Music"),
+                text("1.png"),
+                text("Some kind of address"),
+                text("NCR Delhi"));
+    }
+}
